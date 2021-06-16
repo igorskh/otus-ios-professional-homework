5a4f2c465673d16dbc54373ba3677cda32c5798f
Igor Kim <igor.skh@gmail.com>
2021-06-17 00:41:18 +0200

    Initial Commit

---
+++ otus-ios-homework1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  otus-ios-homework1
+//
+//  Created by Igor Kim on 17.06.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ otus-ios-homework1/otus_ios_homework1App.swift
@@ -0,0 +1,17 @@
+//
+//  otus_ios_homework1App.swift
+//  otus-ios-homework1
+//
+//  Created by Igor Kim on 17.06.21.
+//
+
+import SwiftUI
+
+@main
+struct otus_ios_homework1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
