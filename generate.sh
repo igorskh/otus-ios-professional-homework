8a0d03d205bb84da254b483a8f0862e9e4784f3e
Igor Kim <igor.skh@gmail.com>
2021-08-25 14:35:14 +0200

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,5 +1,16 @@
-SWIFT_MODULE_SRC="Modules/Networking/Sources/Networking/"
-openapi-generator generate -i "metaweather.yaml" -g swift5 -o "api-mobile"
-rm -r $SWIFT_MODULE_SRC""*
-cp -R "api-mobile/OpenAPIClient/Classes/OpenAPIs/". $SWIFT_MODULE_SRC
-rm -r "api-mobile"
+#!/bin/sh
+API_LIST=("metaweather" "newsapi")
+SWIFT_MODULE_SRC="Modules/Networking/Sources"
+
+for a in ${API_LIST[@]}; do
+  openapi-generator generate -i "$a.yaml" -g swift5 -o $a
+done
+
+rm -r $SWIFT_MODULE_SRC
+
+mkdir $SWIFT_MODULE_SRC
+
+for a in ${API_LIST[@]}; do
+  cp -R "$a/OpenAPIClient/Classes/OpenAPIs/". $SWIFT_MODULE_SRC/$a
+  rm -r $a
+done
