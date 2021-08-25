af6bdcbb1846da4190fc87687520ef1fbd21d395
Igor Kim <igor.skh@gmail.com>
2021-08-25 14:38:07 +0200

    update networking import

--- otus-ios-homework1/Extensions/Location+Identifiable.swift
+++ otus-ios-homework1/Extensions/Location+Identifiable.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-import Networking
+import MetaweatherAPI
 
 extension Location: Identifiable {
     public var id: Int { woeid }
--- otus-ios-homework1/Extensions/Location+coordinates.swift
+++ otus-ios-homework1/Extensions/Location+coordinates.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-import Networking
+import MetaweatherAPI
 import CoreLocation
 
 extension Location {
