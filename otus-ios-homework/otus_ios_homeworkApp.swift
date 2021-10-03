3caffcc0f81907862bdb6db2a8adedeb18d20f2d
Igor Kim <igor.skh@gmail.com>
2021-10-03 19:30:18 +0200

    initial

---
+++ otus-ios-homework/otus_ios_homeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  otus_ios_homeworkApp.swift
+//  otus-ios-homework
+//
+//  Created by Igor Kim on 03.10.21.
+//
+
+import SwiftUI
+
+@main
+struct otus_ios_homeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
